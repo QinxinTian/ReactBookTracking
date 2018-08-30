e2f619a817a231d791ea5423eec9cbb0bc18135b
QinxinTian <aptsubl@gmail.com>
2018-08-30 13:31:40 -0700

    Update shelf.

--- src/App.js
+++ src/App.js
@@ -15,28 +15,61 @@ export default class BooksApp extends React.Component {
           }
       }
 
-//Function is called right before the first render.
       componentDidMount() {
-      BooksAPI.getAll()
-      .then(books => this.setState({ books }))
-      .catch(errorMessage);
-   }
-      onShelfChange = (book, shelf) => {
-        BooksAPI.update(book, shelf)
-            .then(
-                this.setState((state) => ({
-                    books: state.books.map(b => {
-                        if (b.title === book.title) {
-                            b.shelf = shelf;
-                            return b
-                        } else {
-                            return b
-                        }
-                    }),
-                    loading: false
-                }))
-            )
-    };
+          BooksAPI.getAll()
+            .then(books => this.setState({ books }))
+            .catch(errorMessage);
+        }
+
+        componentDidUpdate(_, prevState) {
+
+          if (prevState.books.length !== this.state.books.length) {
+
+            BooksAPI.getAll()
+              .then(books => this.setState({ books }))
+              .catch(errorMessage);
+          }
+        }
+
+        /**
+         changeBook - The book to manipulate
+         newValue - The new shelf value for the book
+         */
+        changeShelf(changeBook, newValue) {
+          // Push change to through API
+          BooksAPI.update(changeBook, newValue)
+          .catch(errorMessage);
+
+          this.setState(prevState => {
+            let books = []; // For modifying data before pushing to state
+
+            // Book to change was not in the previous state
+            if (!prevState.books.includes(changeBook)) {
+              changeBook['shelf'] = newValue;                 // Add and set the new shelf value
+              books.concat(prevState.books).push(changeBook); // Combine with the previous state
+
+              addMessage(newValue);
+            }
+            else {
+              // Return a modified array from previous state
+              books = prevState.books.map(book => {
+
+                if (book === changeBook) {
+                  book.shelf = newValue;  // Modify the shelf value
+                  return book;            // Push into new array
+                }
+                else {
+                  return book;            // Otherwise, push unmodified
+                }
+              })
+
+              moveMessage(newValue);
+            }
+
+            return { books }; // Pass new value to the state
+          });
+
+        }
     addBookToShelf(book, shelf) {
         book.shelf = shelf
         this.setState({ [shelf]: this.state[shelf].concat(book) })
